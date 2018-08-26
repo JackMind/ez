42beaf8573a1449e8e369c10b8dd1e73c88f9d71
Rúben Santos <rsantos@Rubens-MBP.home>
2018-08-27 00:42:39 +0100

    fixed bug of gg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('ready',() => { 
 
 var prefix = "$"
 var tardsList = [];
-const roles = ["Carry", "Support", "Jungler", "Roamer", "Nuker", "Initiator", "Pusher"];
+var roles = ["Carry", "Support", "Jungler", "Roamer", "Nuker", "Initiator", "Pusher"];
 
 client.on('message', message => {
 	let args = message.content.split(' ').slice(1);
@@ -19,49 +19,35 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix)) return;
 	if (message.author.bot) return;
 
-  console.log("Received #" + message.id + ": " + message.content);
 	if (message.content.startsWith(prefix + 'ez')) {
     if(tardsList.includes(message.author.username)){
       message.channel.send("You are already in queue fatard @" + message.author.username)
-      .then(message => console.log("Sent #" + message.id + ": " + message.content))
-      .catch(console.error);
       return;
     }
     tardsList.push(message.author.username);
-    message.channel.send("Fatard @" + message.author.username + " joinned the queue ")
-    .then(message => console.log("Sent #" + message.id + ": " + message.content))
-    .catch(console.error);
+    message.channel.send("Fatard @" + message.author.username + " joinned the queue " + tardsList.length +"/5")
 	}else if (message.content.startsWith(prefix + 'gg') || tardsList.length === 5){
     if(tardsList.length === 0){
       message.channel.send("No one in queue")
-      .then(message => console.log("Sent #" + message.id + ": " + message.content))
-      .catch(console.error);
       return;
     }
-    do {
+		while (tardsList.length > 0) {
       var tardRoleNum = Math.floor((Math.random() * roles.length) + 0);
-      var tard = tardsList.pop();
-      message.channel.send(tard+ " is " +roles[tardRoleNum])
-      .then(message => console.log("Sent #" + message.id + ": " + message.content))
-      .catch(console.error);
-      roles.splice(tardRoleNum);
-    } while (tardsList.length > 0);
+				var tard = tardsList.pop();
+	      message.channel.send(tard+ " is " +roles[tardRoleNum])
+	      roles.splice(tardRoleNum, 1);
+				roles.sort();
+    }
     roles = ["Carry", "Support", "Jungler", "Roamer", "Nuker", "Initiator", "Pusher"];
     tardsList = [];
   }else if (message.content.startsWith(prefix + 'call')){
     message.channel.send(":feelsamazingman: :mega:  D O T O  C A L L  @everyone")
-    .then(message => console.log("Sent #" + message.id + ": " + message.content))
-    .catch(console.error);
   }else if(message.content.startsWith(prefix + 'dotoislife')){
-    message.channel.send("!play https://www.youtube.com/watch?v=12vh55_1ul8")
-    .then(message => console.log("Sent #" + message.id + ": " + message.content))
-    .catch(console.error);
+		message.channel.send("!play https://www.youtube.com/watch?v=12vh55_1ul8")
   }else{
     message.channel.send("ez to join\ngg to pick roles\ncall to doto calling\ndotoislife for motivation")
-    .then(message => console.log("Sent #" + message.id + ": " + message.content))
-    .catch(console.error);
   }
 
 });
 //process.env.TOKEN
-client.login(process.env.TOKEN);
+client.login(token);
