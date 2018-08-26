2cafbd834ae84f9acdb31068195cfe6d2e78a85b
Rúben Santos <rsantos@Rubens-MacBook-Pro.local>
2018-08-26 19:38:05 +0100

    fixes for now

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./settings.json').token;
 
+require('http').createServer().listen(3000)
+
 client.on('ready',() => { 
 	console.log('I\'m Online\nI\'m Online');
 });
