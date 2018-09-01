4f7e7d49eb725658a27e06413e322115f0b3cd18
Rúben Santos <rsantos@Rubens-MacBook-Pro.local>
2018-09-01 12:57:01 +0100

    playing doto song auto

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const Commando = require('discord.js-commando');
 const token = require('./settings.json').token;
+const client = new Discord.Client();
+const bot = new Commando.Client();
+const commando = require('discord.js-commando');
+const YTDL = require('ytdl-core');
+require('http').createServer().listen(3000);
 
-require('http').createServer().listen(3000)
+const dotoIsLife = "https://www.youtube.com/watch?v=12vh55_1ul8";
+
+global.currentTeamMembers = [];
+global.servers = {};
+
+function Play(connection, message){
+  var server = servers[message.guild.id];
+  server.dipatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+  server.queue.shift();
+  server.dipatcher.on("end", function(){
+    if(server.queue[0]){
+      Play(connection, message);
+    }else{
+      connection.disconnect();
+    }
+  })
+}
 
 client.on('ready',() => { 
 	console.log('I\'m Online\n');
 });
 
 var prefix = "$"
-var tardsList = ["a","b","c","d"];
+var tardsList = [];
 var roles = ["Pos1", "Pos2", "Pos3", "Pos4", "Pos5"];
-let frases = [["Pelo menos não é jackmind a dar carry", "Spectre, Luna, AM, PL, Juggernaut"],
+var frases = [["Pelo menos não é jackmind a dar carry", "Spectre, Luna, AM, PL, Juggernaut"],
 ["May the odds be with you","Tinker, QOP, Storm Spirit, Zeus, Inv"],
 ["Enchantress, Bristleback, Centaur, Dark Seer, Undying, Phoenix"],
 ["Se eu vir wards em stock és banido do discord", "Bara, Bounty, Tusk, Pudge"],
@@ -19,8 +40,8 @@ let frases = [["Pelo menos não é jackmind a dar carry", "Spectre, Luna, AM, PL
 
 client.on('message', message => {
 
-	let pos = message.content.split(' ').slice(1, 2);
-	let args = message.content.split(' ').slice(2);
+	var pos = message.content.split(' ').slice(1, 2);
+	var args = message.content.split(' ').slice(2);
 	var result = args.join(' ');
 
 	if (!message.content.startsWith(prefix)) return;
@@ -28,23 +49,21 @@ client.on('message', message => {
 
 	if (message.content.startsWith(prefix + 'ez')) {
     if(tardsList.includes(message.author.username)){
-      message.channel.send("You are already in queue fatard @" + message.author.username)
-      return;
+      return message.channel.send("You are already in queue fatard @" + message.author.username)
     }
     tardsList.push(message.author.username);
     message.channel.send("Fatard @" + message.author.username + " joinned the queue " + tardsList.length +"/5")
 	}else if (message.content.startsWith(prefix + 'gg') || tardsList.length === 5){
     if(tardsList.length === 0){
-      message.channel.send("No one in queue")
-      return;
+      return message.channel.send("No one in queue")
     }
-		let ggEz = new Discord.RichEmbed()
+		var ggEz = new Discord.RichEmbed()
 		.setTitle("This is the match:")
-		.setColor(0x00AE86)
 		while (tardsList.length > 0) {
       var tardRoleNum = Math.floor((Math.random() * roles.length) + 0);
 				var tard = tardsList.pop();
 				//FIX: Getting index from string Pos1-5
+				console.log(tard + " is " + roles[tardRoleNum], frases[roles[tardRoleNum].substring(3)-1][Math.floor(Math.random(0, frases[frases[tardRoleNum].length]))])
 				ggEz.addField(tard + " is " + roles[tardRoleNum], frases[roles[tardRoleNum].substring(3)-1][Math.floor(Math.random(0, frases[frases[tardRoleNum].length]))])
 				roles.splice(tardRoleNum, 1);
 				roles.sort();
@@ -52,10 +71,27 @@ client.on('message', message => {
 		message.channel.send(ggEz)
     roles = ["Pos1", "Pos2", "Pos3", "Pos4", "Pos5"];
     tardsList = [];
-  }else if (message.content.startsWith(prefix + 'call')){
-    message.channel.send("<:feelsamazingman:448445382599507969> :mega:  D O T O  C A L L  @everyone")
-  }else if(message.content.startsWith(prefix + 'dotoislife')){
-		message.channel.send("!play https://www.youtube.com/watch?v=12vh55_1ul8")
+  }else if (message.content.startsWith(prefix + 'dotoislife')){
+		console.log("Doto call")
+    message.channel.send("<:feelsamazingman:448445382599507969> :mega:  D O T O  C A L L  <@&418000398914420746>")
+  }else if(message.content.startsWith(prefix + 'call')){
+		console.log("Play doto call")
+	    if(message.member.voiceChannel){
+	      if(!message.guild.voiceConnection){
+	        if(!servers[message.guild.id]){
+	          servers[message.guild.id] = {queue: []}
+	        }
+	        message.member.voiceChannel.join()
+	        .then(connection => {
+	          message.reply("<:feelsamazingman:448445382599507969> :mega:  D O T O  C A L L  <@&418000398914420746>");
+	          var server = servers[message.guild.id];
+	          server.queue.push(dotoIsLife);
+	          Play(connection, message);
+	        })
+	      }
+	    } else {
+	      message.reply("You must be on voice channel")
+	    }
   }else if(message.content.startsWith(prefix + 'add')){
 		if(!roles.includes(pos.toString())){
 			message.channel.send("Usage: add {Pos} message")
@@ -64,22 +100,23 @@ client.on('message', message => {
 			return message.channel.send("Try to write something")
 		}else{
 			frases[roles.indexOf(pos.toString())].push(result.toString())
-			let added = new Discord.RichEmbed()
+			var added = new Discord.RichEmbed()
 			.setTitle("Message added")
 			.addField(pos.toString(), result.toString())
 			message.channel.send(added)
+			console.log(added);
 		}
 		console.log(pos + " " + result)
 		console.log(roles.indexOf(pos.toString()))
 	}else if(message.content.startsWith(prefix + 'ls')){
-		let embed = new Discord.RichEmbed()
+		var embed1 = new Discord.RichEmbed()
 		.setTitle("List of all messages")
 		.addField(roles[0],frases[0])
 		.addField(roles[1],frases[1])
 		.addField(roles[2],frases[2])
 		.addField(roles[3],frases[3])
 		.addField(roles[4],frases[4])
-		message.channel.send(embed)
+		message.channel.send(embed1)
 	}else if(message.content.startsWith(prefix + 'rm')){
 		if(!roles.includes(pos.toString())){
 			message.channel.send("Usage: $rm {Pos} {indexOf}")
@@ -88,14 +125,14 @@ client.on('message', message => {
 			console.log(roles.indexOf(pos.toString()) + " only have: " + frases[roles.indexOf(pos.toString())].length)
 			return message.channel.send(roles.indexOf(pos.toString()) + ":" + frases[frases[roles.indexOf(pos.toString())].length])
 		}else{
-			let removed = new Discord.RichEmbed()
+			var removed = new Discord.RichEmbed()
 			.setTitle("Message removed")
 			.addField(pos.toString(), frases[roles.indexOf(pos.toString())][args[0]])
 			message.channel.send(removed)
 			frases[roles.indexOf(pos.toString())].splice(args[0].toString(), 1)
 		}
 	}else{
-		let embed = new Discord.RichEmbed()
+		var embed2 = new Discord.RichEmbed()
 		.setTitle("Usage")
 		.addField("Joining queue", "$ez")
 		.addField("To pick roles", "$gg")
@@ -104,9 +141,10 @@ client.on('message', message => {
 		.addField("Add new message", "$add {Pos} message")
 		.addField("Remove messages", "$rm {Pos} {indexOf}")
 		.addField("List messages", "$ls")
-		message.channel.send(embed)
+		message.channel.send(embed2)
   }
 });
 
 //process.env.TOKEN
 client.login(token);
+bot.login(token);
